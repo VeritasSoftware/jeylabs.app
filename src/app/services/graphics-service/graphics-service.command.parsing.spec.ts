092f0f828a59caf963f082a77651a7706c63694b
VeritasSoftware <shantanu_negi@hotmail.com>
2018-05-10 01:50:48 +1000

    added unit tests for triangle and pentagon

--- src/app/services/graphics-service/graphics-service.command.parsing.spec.ts
+++ src/app/services/graphics-service/graphics-service.command.parsing.spec.ts
@@ -9,8 +9,19 @@ describe('GraphicsService', () => {
     }).compileComponents();
   }));
 
-  it('should parse a Square natural command', () => {
+  it('should parse a Triangle natural command', () => {    
+    var graphicsService = new GraphicsService();
+    var result = graphicsService.Draw("Draw a triangle with a side length of 200");
     
+    expect(result.name == "triangle").toBeTruthy();
+    expect(result.shapeType == ShapeType.triangle).toBeTruthy();
+    expect(result.measurementAmounts.length > 0).toBeTruthy();
+    expect(result.measurementAmounts[0].measurement = "side length").toBeTruthy();
+    expect(result.measurementAmounts[0].amount = 200).toBeTruthy();
+    expect(result.points.length == 3).toBeTruthy();
+  });
+
+  it('should parse a Square natural command', () => {    
     var graphicsService = new GraphicsService();
     var result = graphicsService.Draw("Draw a square with a side length of 200");
     
@@ -20,5 +31,17 @@ describe('GraphicsService', () => {
     expect(result.measurementAmounts[0].measurement = "side length").toBeTruthy();
     expect(result.measurementAmounts[0].amount = 200).toBeTruthy();
     expect(result.points.length == 4).toBeTruthy();
+  });
+
+  it('should parse a Pentagon natural command', () => {    
+    var graphicsService = new GraphicsService();
+    var result = graphicsService.Draw("Draw a pentagon with a side length of 200");
+    
+    expect(result.name == "pentagon").toBeTruthy();
+    expect(result.shapeType == ShapeType.pentagon).toBeTruthy();
+    expect(result.measurementAmounts.length > 0).toBeTruthy();
+    expect(result.measurementAmounts[0].measurement = "side length").toBeTruthy();
+    expect(result.measurementAmounts[0].amount = 200).toBeTruthy();
+    expect(result.points.length == 5).toBeTruthy();
   });
 });
